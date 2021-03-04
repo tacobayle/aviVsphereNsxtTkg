4cb1bc3c75284f0e521c889ff6d0eb5b939e8f6f
tacobayle <nicolas.bayle@gmail.com>
2021-03-04 14:21:25 +0000

    20210304

--- variables.tf
+++ variables.tf
@@ -63,7 +63,7 @@ variable "ansible" {
   default = {
     version = "2.9.12"
     aviConfigureUrl = "https://github.com/tacobayle/aviConfigure"
-    aviConfigureTag = "v4.36"
+    aviConfigureTag = "v4.37"
   }
 }
 
