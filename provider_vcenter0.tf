76e24af9a8155a5f65ab33748872f9fda76b5e39
tacobayle <nicolas.bayle@gmail.com>
2021-04-20 14:53:02 +0000

    20210420

--- provider_vcenter0.tf
+++
@@ -1,9 +0,0 @@
-
-provider "vsphere" {
-  user                 = "admin"
-  password             = "password1"
-  vsphere_server       = "10.8.0.10"
-  alias                = "vcenter0"
-  allow_unverified_ssl = true
-}
-
