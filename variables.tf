2f58d0e0734d19bb4bdde5fad354fdd61930e572
tacobayle <nicolas.bayle@gmail.com>
2021-04-27 12:37:59 +0000

    20210427

--- variables.tf
+++ variables.tf
@@ -105,7 +105,7 @@ variable "ansible" {
   default = {
     version = "2.9.12"
     aviConfigureUrl = "https://github.com/tacobayle/aviConfigure"
-    aviConfigureTag = "v5.46"
+    aviConfigureTag = "v5.47"
     aviPbAbsentUrl = "https://github.com/tacobayle/ansiblePbAviAbsent"
     aviPbAbsentTag = "v1.51"
     nsxtConfigureDfwUrl = "https://github.com/tacobayle/ansibleNsxtConfigureDfw"
